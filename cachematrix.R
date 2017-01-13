cfbe74abc0e8f90ab4a8523c21a78bfb0f11afa1
Mohammed Ait-Oufkir <mohammed.aitoufkir@gmail.com>
2017-01-13 17:52:01 +0100

    Submit the first change of the 2nd Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## defines setters and getters for Matrix and its inverse, it takes as a parameter
+## a matrix we assume that all the provided matrices can be inverted
+makeCacheMatrix <- function(mtx = matrix()) {
+    inverseMtx <- NULL
+    set <- function(initMtx) {
+        mtx <<- initMtx
+        inverseMtx <<- NULL
+    }
+    get <- function() mtx
+    setInverse <- function(invMtx) inverseMtx <<- invMtx
+    getInverse <- function() inverseMtx
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve takes a matrix container that corresponds to the list with getters
+## and setters returned by makeCacheMatrix it checks if the inverse matrix exists
+## if yes it returns it from the cache otherwise it calculates the inverse Matrix
+## using solve function
+
+cacheSolve <- function(mtxContainer, ...) {
+    ## first check for the inverse of the matrix is it is not NULL
+    inverseMtx <- mtxContainer$getInverse()
+    if (!is.null(inverseMtx)) {
+        message("getting the cached inverse of the matrix")
+        return(inverseMtx)
+    }
+    ## else calculate the inverse of the matrix
+    mtx <- mtxContainer$get()
+    inverseMtx <- solve(mtx)
+    mtxContainer$setInverse(inverseMtx)
+    message("calculating the inverse of the matrix")
+    ## Return a matrix that is the inverse of 'mtx'
+    inverseMtx
 }
